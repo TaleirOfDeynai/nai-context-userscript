3d95369465a0a879476fac1eef0bd58a63eb276e
Taleir of Deynai <TaleirOfDeynai@live.com>
2022-06-05 15:53:07 -0700

    Adds a test for fragment stability for `TrimmingProviders`.

--- src/contextBuilder/TrimmingProviders.spec.ts
+++ src/contextBuilder/TrimmingProviders.spec.ts
@@ -1,14 +1,16 @@
 import { describe, it, expect } from "@jest/globals";
 import { mockFragment } from "@spec/helpers-splitter";
+import { withComments } from "@spec/mock-story";
 
+import _zip from "lodash/zip";
 import { dew } from "@utils/dew";
-import { chain, interweave, flatMap, iterReverse } from "@utils/iterables";
+import { chain, interweave, flatMap, iterReverse, skipRight } from "@utils/iterables";
 import $TrimmingProviders from "./TrimmingProviders";
 import AppConstants from "@nai/AppConstants";
 
 import type { TextAssembly } from "./TextAssembly";
 import type { TextFragment } from "./TextSplitterService";
-import type { TrimProvider } from "./TrimmingProviders";
+import type { TrimProvider, SplitterFn } from "./TrimmingProviders";
 
 const fakeRequire: any = (module: any) => {
   switch (module) {
@@ -139,43 +141,99 @@ describe("comment removal", () => {
 
   const toOutput = (
     fragments: Iterable<TextFragment>,
-    provider: TrimProvider
+    providerFn: SplitterFn,
+    reversed: boolean
   ): string => {
     return chain(fragments)
-      .thru((iter) => flatMap(iter, provider.newline))
+      .thru((iter) => flatMap(iter, providerFn))
       .map((frag) => frag.content)
-      .thru((iter) => provider.reversed ? iterReverse(iter) : iter)
+      .thru((iter) => reversed ? iterReverse(iter) : iter)
       .value((iter) => [...iter].join(""));
   };
 
   /** Performs some basic tests with the given `provider`. */
-  const basicTests = (provider) => {
-    it("should pre-process by extracting the text assembly's content", () => {
-      const assembly = mockAssembly(simpleTests.baseLines);
-      expect(provider.preProcess(assembly)).toBe(assembly.content);
-    });
-
+  const basicTests = (
+    preProcess: TrimProvider["preProcess"],
+    providerFn: SplitterFn,
+    reversed: boolean
+  ) => {
     for (const theCase of simpleTests.cases) {
       it(`should ${theCase.should}`, () => {
         const assembly = mockAssembly(theCase.input);
-        const input = provider.preProcess(assembly);
-        expect(toOutput(input, provider)).toBe(theCase.expected);
+        const input = preProcess(assembly);
+        expect(toOutput(input, providerFn, reversed)).toBe(theCase.expected);
       });
     }
   };
 
+  const fragmentTest = (
+    preProcess: TrimProvider["preProcess"],
+    providerFn: SplitterFn,
+    reversed: boolean
+  ) => {
+    it("should preserve fragment offsets", () => {
+      const assembly = mockAssembly([withComments]);
+      const input = preProcess(assembly);
+      const result = [...flatMap(input, providerFn)];
+
+      const reSplit = reversed ? /\n?(?:## )?.*/g : /(?:## )?.*\n?/g;
+      const reCommentLine = reversed ? /^\n?##/ : /^##/;
+      const checkFrags = chain(withComments.matchAll(reSplit))
+        // Drop lines with comments.
+        .filter(([text]: string[]) => !reCommentLine.test(text))
+        // Break the non-comment lines into fragments of either
+        // `"\n"` or other text.
+        .thru(function*(iter) {
+          for (const match of iter) {
+            const outerOffset = match.index ?? 0;
+            for (const frag of match[0].matchAll(/\n|.*/g)) {
+              if (!frag[0]) continue;
+              const innerOffset = frag?.index ?? 0;
+              yield mockFragment(frag[0], 10 + innerOffset + outerOffset);
+            }
+          }
+        })
+        // Reverse, if needed.
+        .thru((iter) => reversed ? iterReverse(iter) : iter)
+        // The last fragment is a `"\n"` that should not exist in the result.
+        .thru((iter) => skipRight(iter, 1))
+        .toArray();
+
+      expect(result).toEqual(checkFrags);
+    });
+  };
+
   describe("`trimBottom` provider", () => {
-    basicTests(providers.removeComments.trimBottom);
+    const provider = providers.removeComments.trimBottom;
+    const { preProcess, newline, reversed } = provider;
+
+    it("should pre-process by extracting the text assembly's content", () => {
+      const assembly = mockAssembly(simpleTests.baseLines);
+      expect(preProcess(assembly)).toBe(assembly.content);
+    });
+
+    basicTests(preProcess, newline, reversed);
+    fragmentTest(preProcess, newline, reversed);
   });
 
   describe("`trimTop` provider", () => {
-    basicTests(providers.removeComments.trimTop);
+    const provider = providers.removeComments.trimTop;
+    const { preProcess, newline, reversed } = provider;
+
+    it("should pre-process by extracting the text assembly's content", () => {
+      const assembly = mockAssembly(simpleTests.baseLines);
+      expect(preProcess(assembly)).toBe(assembly.content);
+    });
+
+    basicTests(preProcess, newline, reversed);
+    fragmentTest(preProcess, newline, reversed);
   });
 
   describe("`doNotTrim` provider", () => {
     // This one functions a little differently, as it changes how
     // `preProcess` works, but it ultimately defers to `trimBottom`.
     const provider = providers.removeComments.doNotTrim;
+    const { preProcess, newline, reversed } = provider;
 
     for (const theCase of simpleTests.cases) {
       it(`should ${theCase.should}`, () => {
@@ -185,9 +243,12 @@ describe("comment removal", () => {
       });
     }
 
+    // Using a faux `SplitterFn` here so we only test `preProcess`.
+    fragmentTest(preProcess, (frag) => [frag], reversed);
+
     it("should have a noop `newline` method", () => {
       const fragments = mockAssembly(simpleTests.baseLines).content;
-      expect(toOutput(fragments, provider)).toBe("");
+      expect(toOutput(fragments, newline, reversed)).toBe("");
     });
   });
 });
